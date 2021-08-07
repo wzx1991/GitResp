f232890ab92514a87f26fc23c2edc86ba8b81cad
wengzx <495514464@qq.com>
2021-08-07 12:54:41 +0800

    修改了第一次的文件

--- TestMode01/src/com/msb/Student.java
+++ TestMode01/src/com/msb/Student.java
@@ -7,4 +7,15 @@ package com.msb;
  * @Description:com.msb
  */
 public class Student {
+    private int age;
+    private double height ;
+    private String name ;
+
+
+    public Student(int age, double height, String name) {
+        this.age = age;
+        this.height = height;
+        this.name = name;
+    }
+
 }
