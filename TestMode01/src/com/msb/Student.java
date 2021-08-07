a0916c865a92f42d5c4695e39a3ff8d61bf51082
wengzx <495514464@qq.com>
2021-08-07 12:58:52 +0800

    修改了第二次的文件

--- TestMode01/src/com/msb/Student.java
+++ TestMode01/src/com/msb/Student.java
@@ -18,4 +18,27 @@ public class Student {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
